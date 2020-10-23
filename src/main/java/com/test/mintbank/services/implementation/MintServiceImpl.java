13eed6829cdf29c8161d7480fcb4e8977070d650
olumide30 <ogundeleolumuyiwaolumide@gmail.com>
2020-10-23 03:37:49 +0100

    app config and getCardDetails

--- src/main/java/com/test/mintbank/services/implementation/MintServiceImpl.java
+++ src/main/java/com/test/mintbank/services/implementation/MintServiceImpl.java
@@ -6,25 +6,48 @@ import com.test.mintbank.services.MIntService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
 public class MintServiceImpl implements MIntService {
 
+    @Value("binListUrl")
+    private String binListUrl;
+
     @Autowired
     private RestTemplate restTemplate;
 
-    @Value("")
+
 
     @Override
     public CardDetails getCardDetails(Long cardNumber) {
+        CardDetails cardDetails = new CardDetails();
+        try{
+            final String uri = "https://lookup.binlist.net/"+cardNumber;
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+            headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+            headers.add("Access-Control-Allow-Origin", "*");
+            headers.add("User-Agent", "Mozilla");
 
-        final String uri = "http://lookup.binlist.net/"+cardNumber;
-        HttpHeaders headers = new HttpHeaders();
-        headers
-        return null;
+            HttpEntity<?> http = new HttpEntity<>(headers);
+            ResponseEntity<CardDetails> response = restTemplate.exchange(uri, HttpMethod.GET,http,CardDetails.class);
+            if(response.getStatusCode().is2xxSuccessful() && response.getBody() != null){
+                return response.getBody();
+            }else{
+                log.info("Result Not Found");
+            }
+        }catch (HttpClientErrorException e){
+            log.info("Client Exception "  + e.getMessage());
+        }
+        catch (Exception e){
+            log.info("Server Exception " + e.getMessage());
+        }
+
+        return cardDetails;
     }
 }
